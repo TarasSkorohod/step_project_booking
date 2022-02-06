be62941d0441fb869c6a1116223054ced5d93a79
tarasskorohod <skorohodit12@gmail.com>
2022-02-06 20:36:34 +0200

    test3

--- scr/main1.java
+++ scr/main1.java
@@ -2,15 +2,6 @@ import java.util.Scanner;
 
 public class main1{
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Веди команду выход:");
-    while(true) {
-      String str = sc.next();
-
-
-      if("выход".equals(str)) {
-        break;
-      }
+    System.out.println("hello");
     }
-  }
 }
