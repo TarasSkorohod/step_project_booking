45091177f81b2f018d7139fd77c3b3ae7dccd672
SinkLine_P <popovartem505@gmail.com>
2022-02-07 17:38:26 +0200

    add controllers - createNewFlight, getFlightByIndex, displayAllFlight

--- scr/controllers/FlightController.java
+++ scr/controllers/FlightController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import objects.Flight;
 import services.FlightService;
 
 public class FlightController {
@@ -7,5 +8,15 @@ public class FlightController {
     FlightService.getAllFlight();
   }
 
+  public static void displayAllFlight() {
+    FlightService.displayAllFlight();
+  }
 
+  public static void getFlightByIndex(int index) {
+    FlightService.getFlightByIndex(index);
+  }
+
+  public static Flight createNewFlight(String destination, int day, int month, int year, int countPeople) {
+    return FlightService.createNewFlight(destination, day, month, year, countPeople);
+  }
 }
