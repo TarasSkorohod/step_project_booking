6155b7ed24ed1a78560d7696880b2dc44b395039
SinkLine_P <popovartem505@gmail.com>
2022-02-07 15:55:45 +0200

    fix merge

--- scr/controllers/FlightController.java
+++ scr/controllers/FlightController.java
@@ -4,7 +4,8 @@ import services.FlightService;
 
 public class FlightController {
   public static void getAllFlight() {
-    FlightService.getAllFlight();
+
+//    FlightService.getAllFlight();
   }
 
 
