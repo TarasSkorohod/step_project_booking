7577b92a4789ad072695c54c0c84af10ad486914
tarasskorohod <skorohodit12@gmail.com>
2022-02-15 18:59:34 +0200

    delete_user_login

--- src/main/java/DAO/AuthDAO.java
+++
@@ -1,8 +0,0 @@
-package main.java.DAO;
-
-import main.java.authorization.User;
-
-public interface AuthDAO {
-  public abstract boolean isLogin(String username,String passworld);
-  public abstract void regist(User user);
-}
