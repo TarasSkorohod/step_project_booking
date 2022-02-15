ea7a6869471789e6f4bfe4e06e5f01a2c3c44233
tarasskorohod <skorohodit12@gmail.com>
2022-02-15 19:05:46 +0200

    appStart

--- src/main/java/StartApp.java
+++ src/main/java/StartApp.java
@@ -1,15 +1,21 @@
 package main.java;
 
 import main.java.logger.Logs;
+import main.java.objects.BookingApp;
 
 import java.io.FileNotFoundException;
 import java.text.ParseException;
 
 public class StartApp {
   public static void main(String[] args) throws FileNotFoundException, ParseException {
+    BookingApp app = new BookingApp();
+
     Logs.log("Set test flights", "info");
     SetTestFlights.setTestFlights();
     Logs.log("App is run", "info");
-    App.app();
+    Logs.log("Run auth", "info");
+
+    Logs.log("Start app", "info");
+    app.start();
   }
 }
